fda20ce728fd3ca32f7f8c58aaab7b196989c5dc
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-03-10 17:03:22 -0600

    chore(js): limit svg search

--- docs/js/modules/swapImgSvgWithRawSvg.js
+++ docs/js/modules/swapImgSvgWithRawSvg.js
@@ -1,6 +1,6 @@
 import 'https://cdn.jsdelivr.net/npm/@iconfu/svg-inject@1.2.3/dist/svg-inject.min.js';
 
 /* To change any <img> loading a .svg to be that .svg inline */
-SVGInject( document.querySelectorAll('img[src$=".svg"]'), {
+SVGInject( document.querySelectorAll('.logo'), {
   onAllFinish: () => console.info('Image SVGs replaced with inline SVGs.')
 });
