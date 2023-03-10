f870519fc7f9a437906541ee0588d3b92a5158dd
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-03-10 16:59:46 -0600

    chore(js): remove superfluous characters

--- docs/js/modules/swapImgSvgWithRawSvg.js
+++ docs/js/modules/swapImgSvgWithRawSvg.js
@@ -2,7 +2,5 @@ import 'https://cdn.jsdelivr.net/npm/@iconfu/svg-inject@1.2.3/dist/svg-inject.mi
 
 /* To change any <img> loading a .svg to be that .svg inline */
 SVGInject( document.querySelectorAll('img[src$=".svg"]'), {
-  onAllFinish: function() {
-      console.info('Image SVGs replaced with inline SVGs.');
-  }
+  onAllFinish: () => console.info('Image SVGs replaced with inline SVGs.')
 });
