9dcf969670dd0e777bcfb576c2572babc71473b1
Susan Lindsey <slindsey@tacc.utexas.edu>
2023-11-27 15:24:21 -0600

    Created /archives subdir for old documentation, excluded from searches Archived Maverick2 UG & associated include files Archived iRODS UG

--- docs/archives/2mav/configurestrings.sh
+++ docs/archives/2mav/configurestrings.sh
@@ -43,6 +43,18 @@ then
 	# sed	-e "s/MACHINENAME/$machinename/g" \
 	# 	-e "s/STRIPING/$striping/g"  < conductsrc.md > $conductoutputfile
 
+### Maverick2 
+#elif [ "$1" = "maverick2" ] 
+#then
+#	echo "building includes for Maverick2 user guide"
+#	machinename="Maverick2"
+#	helpoutputfile="maverick2-help.md"
+#	jobaccountingoutputfile="maverick2-jobaccounting.md"
+#
+#	sed	-e "s/MACHINENAME/$machinename/g" \
+#		-e "s/TACCINFOPATH/$taccinfopath/g" < jobaccountingsrc.md > $jobaccountingoutputfile
+#	sed	-e "s/MACHINENAME/$machinename/g" < helpsrc.md > $helpoutputfile
+
 
 ### Stampede2
 elif [ "$1" = "stampede2" ] 
